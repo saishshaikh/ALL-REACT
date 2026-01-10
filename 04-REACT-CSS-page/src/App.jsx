f87c2cb2ba5e49fb6a469f05d59c4a1af61bb2f1
saishshaikh <tumhara_email@example.com>
2026-01-10 21:56:50 +0530

    REACR=CSS-PAGES

---
+++ 04-REACT-CSS-page/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Component/Button";  // Correct relative path
+import "./style/Button.css";
+
+function App() {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ 04-REACT-CSS-page/src/Component/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../style/Button.css";
+
+const  Button = () => {
+  return (
+    <div>
+        <input type="text"  placeholder='YOUR DAIKY TASk'/>
+     <button className='btn'> CLICK ME </button>  
+    </div>
+  )
+}
+
+export default Button;
