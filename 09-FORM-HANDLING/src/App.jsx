34c9aa3330f5ea702e0b824e81aa4866d84e4427
saishshaikh <saishshaikh2323@gmail.com>
2026-01-16 20:37:00 +0530

    FORM_HANDLING

--- 09-FORM-HANDLING/src/App.jsx
+++ 09-FORM-HANDLING/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const formhndler = (e) => {
+    e.preventDefault();
+    console.log("FORM SUBMITTED");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <form onSubmit={(e) => {
+        formhndler(e);
+      }}>
+        <input type="text" placeholder="ENTER YOUR NAME" />
+        <button type="submit">SUBMIT</button>
+      </form>
+    </div>
+  );
+};
 
-export default App
+export default App;
